72e883346ee78e716891cdbb0d8b3a22d54495c3
wackymonkey2 <hbshredz@gmail.com>
2016-04-12 16:25:51 -0400

    Added files via upload

---
+++ main.c
@@ -0,0 +1,126 @@
+#include <pebble.h>
+//#include "effect_layer.h"
+
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+TextLayer *date_layer;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+	static char date_buffer[10];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+	strftime(date_buffer, sizeof(date_buffer), "%B %e", tick_time);
+	
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+	text_layer_set_text(date_layer, date_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void main_window_load(Window *window) {
+	static GFont s_time_font;
+	static GFont s_date_font;
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+	// Create GBitmap
+s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_sierp_triangle);
+
+// Create BitmapLayer to display the GBitmap
+s_background_layer = bitmap_layer_create(bounds);
+
+// Set the bitmap onto the layer and add to the window
+bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+	
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(GRect(0, -5, bounds.size.w, 48));
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorClear);
+	
+  text_layer_set_text(s_time_layer, "00:00");
+	// Create GFont
+s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_orbitron_40));
+
+// Apply to TextLayer
+text_layer_set_font(s_time_layer, s_time_font);
+
+  
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+	date_layer = text_layer_create(GRect(0, 122, 144, 46));
+	text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(date_layer));
+	text_layer_set_background_color(date_layer, GColorClear);
+  	text_layer_set_text_color(date_layer, GColorClear);
+	s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_orbitron_24));
+	text_layer_set_font(date_layer, s_date_font);
+	
+}
+
+static void main_window_unload(Window *window) {
+	// Destroy GBitmap
+gbitmap_destroy(s_background_bitmap);
+
+// Destroy BitmapLayer
+bitmap_layer_destroy(s_background_layer);
+
+	static GFont s_time_font;
+	// Unload GFont
+fonts_unload_custom_font(s_time_font);
+
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+	
+}
+
+
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+window_set_background_color(s_main_window, GColorBlack);
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+
+  // Make sure the time is displayed from the start
+  update_time();
+	
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
+	text_layer_destroy(date_layer);
+
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
